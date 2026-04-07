e5a03a2926ee6fc5292b5d50140088102b0eb9c6
maheensawera <maheensawera1040@gmail.com>
2026-04-07 09:02:10 +0500

    initial commit

--- actions/interview.js
+++ actions/interview.js
@@ -21,7 +21,7 @@ export async function generateQuiz() {
 
 
         const prompt = `
-    Generate 3 technical interview questions for a ${user.industry
+    Generate 10 technical interview questions for a ${user.industry
             } professional${user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
             }.
     
